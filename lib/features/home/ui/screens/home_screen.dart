9b947a7dc0391be9f9d37dad1a00460bee28ce25
Mostafa_Esmail <mostafaesmail9244@gmail.com>
2024-06-13 16:05:55 +0300

    -add film model -add home screen

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,7 +1,9 @@
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/helper/spacing_helper.dart';
 import 'package:movie_app/core/themes/text_styles.dart';
 import 'package:movie_app/core/widgets/background_widget.dart';
+import 'package:movie_app/features/home/data/models/response/film_model.dart';
 import 'package:movie_app/features/home/ui/widgets/carousel_slider_widget.dart';
 import 'package:movie_app/features/home/ui/widgets/custom_app_bar.dart';
 
@@ -18,18 +20,35 @@ class HomeScreen extends StatelessWidget {
               horizontal: 12,
               vertical: 8,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const CustomAppBar(),
-                verticalSpacing(10),
-                Text(
-                  'Popular Now',
-                  style: TextStyles.font13WhiteSemiBold,
-                ),
-                verticalSpacing(10),
-                CarouselSliderWidget(),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CustomAppBar(),
+                  verticalSpacing(10),
+                  Text(
+                    'Popular Now',
+                    style: TextStyles.font13WhiteSemiBold,
+                  ),
+                  verticalSpacing(10),
+                  const CarouselSliderWidget(),
+                  verticalSpacing(20),
+                  //actions
+                  Text('Action', style: TextStyles.font13WhiteSemiBold),
+                  verticalSpacing(5),
+                  ListFilm(films: comedyFilms),
+                  verticalSpacing(20),
+                  //comedy
+                  Text('Fantasy', style: TextStyles.font13WhiteSemiBold),
+                  verticalSpacing(5),
+                  ListFilm(films: comedyFilms),
+                  //fantasy
+                  verticalSpacing(20),
+                  Text('Fantasy', style: TextStyles.font13WhiteSemiBold),
+                  verticalSpacing(5),
+                  ListFilm(films: fantasyFilms),
+                ],
+              ),
             ),
           ),
         ),
@@ -38,3 +57,33 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
+class ListFilm extends StatelessWidget {
+  const ListFilm({
+    Key? key,
+    required this.films,
+  }) : super(key: key);
+  final List<FilmModel> films;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 140,
+      child: ListView.builder(
+        itemBuilder: (context, index) => ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: Padding(
+            padding: EdgeInsets.only(left: index == 0 ? 0 : 6),
+            child: SizedBox(
+              height: 110,
+              width: 112,
+              child: FancyShimmerImage(
+                imageUrl: films[index].image!,
+              ),
+            ),
+          ),
+        ),
+        scrollDirection: Axis.horizontal,
+        itemCount: films.length,
+      ),
+    );
+  }
+}
